cd5a9c9ae1e2dd6166c31d19c1630891b818707f
Ivan Roa Mora <iroam@psl.com.co>
2021-06-10 19:29:02 -0500

    Adding new steps to config Jenkins.

--- provision.sh
+++ provision.sh
@@ -7,8 +7,10 @@ sudo apt install python3-pip -y
 sudo apt-get install ansible -y
 sudo apt-get install mysql-client -y
 sudo python3 -m pip install PyMySQL;
+sudo python3 -m pip install boto3;
+sudo python3 -m pip install botocore;
 cd /home/vagrant/
 sudo runuser -l  vagrant -c 'ansible-galaxy collection install community.mysql'
 sudo chmod 400 /home/vagrant/.ssh/Roam.pem
 sudo echo "[bastion]
-3.101.104.188 ansible_ssh_private_key_file=/home/vagrant/.ssh/Roam.pem ansible_user=ubuntu" > /etc/ansible/hosts
+13.57.254.99 ansible_ssh_private_key_file=/home/vagrant/.ssh/Roam.pem ansible_user=ubuntu" > /etc/ansible/hosts
